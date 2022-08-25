af13a030143c1eba22f19ceffb4ac40d320a4eff
Arturo Velasquez <arturovgr@gmail.com>
2022-08-25 17:21:52 +0100

    last minute changes to module

--- raspberry_pi/collection_module.py
+++ raspberry_pi/collection_module.py
@@ -23,6 +23,8 @@ antena.set_address_bytes(len(nRF_address))
 antena.open_reading_pipe(RF24_RX_ADDR.P1, nRF_address)
 antena.show_registers()
 
+my_pi.set_mode(13,pigpio.OUTPUT)
+my_pi.set_mode(19,pigpio.OUTPUT)
 try:
     print(f'Message using address: {nRF_address}')
     count = 0
@@ -33,17 +35,14 @@ try:
             pipe = antena.data_pipe()
             payload = antena.get_payload()    
 
-                # Resolve protocol number.
+            # Resolve protocol number.
             protocol = payload[0] if len(payload) > 0 else -1            
 
             hex = ':'.join(f'{i:02x}' for i in payload)
 
             # Show message received as hex.
             print(f"{now:%Y-%m-%d %H:%M:%S.%f}: pipe: {pipe}, len: {len(payload)}, bytes: {hex}, count: {count}")
-
-            # If the length of the message is 9 bytes and the first byte is 0x01, then we try to interpret the bytes
-            # sent as an example message holding a temperature and humidity sent from the "simple-sender.py" program.
-            #if len(payload) == 9 and payload[0] == 0x01:
+            # decide what tyoe of module is recieving
             if payload[0] == 0x01:
                 values = struct.unpack("<BBBffhhh", payload)
                 print(f'Module type: {values[0]}, module type: {values[1:3]}, temperature: {values[3]}, humidity: {values[4]}, soil: {values[5]},light: {values[6]}, valve {values[7]}')
@@ -51,7 +50,13 @@ try:
                     typer= csv.writer(file)
                     typer.writerow([measured_time,values[0],values[1]+values[2],round(values[3],2),round(values[4],2),values[5],values[6],values[7],'','','','',''])
                     file.close()
-            if payload[0] == 0x02:
+                # MOVING MOTORS ACCORDING TO THEIR module number and valve status
+                if(int(values[0])%2 == 0):
+                    control_servo(13,values[7])
+                if(int(values[0])%2 == 1):
+                    control_servo(19,values[7])
+           
+            elif payload[0] == 0x02:
                 values = struct.unpack("<BBBhhhff", payload)
                 print(f'Module number: {values[0]}, module type: {values[1:3]}, nitrogen: {values[5]},phosphorus: {values[6]}, potasium {values[7]}, latitude: {values[3]}, longitude: {values[4]}')
                 with open(filename, 'a', newline='') as file:
@@ -65,4 +70,21 @@ except:
     antena.power_down()
     my_pi.stop()
 
+# The valve opens wider if any module requests it otherwise only closes completly upon request.
+def control_servo(motor_num,position):
+    pulse=0
+    if position==0:
+        pulse=1000
+    elif position==1:
+        pulse=1250
+    elif position==2:
+        pulse=1500
+    elif position==3:
+        pulse=1750
+    elif position==4:
+        pulse=2000
+    else:
+        pulse=0
+    if pulse > my_pi.get_servo_pulsewidth(motor_num) & pulse>1000 :
+        my_pi.set_servo_pullsewidth(motor_num,pulse)
 
